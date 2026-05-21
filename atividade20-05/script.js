842b425f660b92611dec39c25d10af7d65644389
zikadevv <fidorc8@gmail.com>
2026-05-20 22:00:40 -0300

    aula1js

---
+++ atividade20-05/script.js
@@ -0,0 +1,14 @@
+function corVermelha() {
+            document.body.style.backgroundColor = 'red';
+        }
+        document.getElementById('red')
+        function corVerde() {
+            document.body.style.backgroundColor = 'green';
+        }
+        document.getElementById('green')
+        function corAzul() {
+            document.body.style.backgroundColor = 'blue';
+        }
+        function mudaCor(cor) {
+            document.body.style.backgroundColor = cor;
+        }
